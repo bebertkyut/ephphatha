babe501b675852249ff47cafa8e5ffbb32bc960f
bebertkyut <bebertkyut@users.noreply.github.com>
2024-11-18 08:08:19 +0000

    deploy: 22eace4e7e2da46ed6d101c1eb3af49661cf759e

--- UserDashboard/content.js
+++ UserDashboard/content.js
@@ -1,6 +1,9 @@
 import { db } from '../firebaseConfig.js';
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
+console.log(redirectTo);  // Should log the function definition
+
+
 // Function that navigates to different content categories
 window.redirectTo = function(category) {
     if (category === 'Single Character') {
