158bb906f99fd7c16a55f0c12b426e30b264aa9d
bebertkyut <bebertkyut@users.noreply.github.com>
2024-11-18 07:53:43 +0000

    deploy: c49484d1bcbdfe0512a0fcaa0ea914a48b6ac801

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyAW65C2w8uxxDw9Va_GFOoCYQUVgm21cM4',
-  authDomain: 'ephphathadb.firebaseapp.com',
-  projectId: 'ephphathadb',
-  storageBucket: 'ephphathadb.appspot.com',
-  messagingSenderId: '408778244868',
-  appId: '1:408778244868:web:43bb14d52f45c4c5424651'
-};
